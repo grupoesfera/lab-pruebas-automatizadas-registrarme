b3a98275745f38424b209fde706c3ee97d6ec0c8
Sebastian Ismael <sebastianismael@gmail.com>
2018-11-16 20:24:21 -0300

    fix

--- src/test/java/ar/com/grupoesfera/pruebas/aceptacion/cucumber/RegistrarmeCucumber.java
+++ src/test/java/ar/com/grupoesfera/pruebas/aceptacion/cucumber/RegistrarmeCucumber.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.core.Serenity;
 import org.openqa.selenium.By;
 
 import static org.assertj.core.api.Assertions.assertThat;
